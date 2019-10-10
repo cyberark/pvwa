6f0dd3fc2ba53c0a3438adacc9f1cfa4a64d2b41
Avishay Bar <avishay.il@gmail.com>
2019-10-10 20:41:17 +0300

    Update generate_inventory.sh

--- tests/inventory/generate_inventory.sh
+++ tests/inventory/generate_inventory.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i -- "s/region_placeholder/${AWS_DEFAULT_REGION}/g" inventory/ec2.ini
-ansible-inventory -i inventory/ec2.py --list tag_kitchen_type_windows --export -y > ./inventory/hosts
+ansible-inventory -i inventory/ec2.py --list tag_kitchen_type_pvwa --export -y > ./inventory/hosts
 echo "Ansible hosts updated successfully"
