120d147dab1945a396d2090ce3864a9517be4314
Avishay Bar <avishay.il@gmail.com>
2019-10-11 08:08:06 +0300

    - Fixed tests

--- tests/inventory/generate_inventory.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i -- "s/region_placeholder/${AWS_DEFAULT_REGION}/g" inventory/ec2.ini
-ansible-inventory -i inventory/ec2.py --list tag_kitchen_type_windows --export -y > ./inventory/hosts
-echo "Ansible hosts updated successfully"
