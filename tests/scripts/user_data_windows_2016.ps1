0cbf8711b3a63aed055ff0c67f1bde22783c4858
slevic <61182664+slevic@users.noreply.github.com>
2021-02-10 13:08:33 +0200

    SHLD-1516: checksum update for 12.1 (#130)

--- tests/scripts/user_data_windows_2016.ps1
+++ tests/scripts/user_data_windows_2016.ps1
@@ -4,9 +4,10 @@ $admin = [adsi]("WinNT://./administrator, user")
 $admin.PSBase.Invoke("SetPassword", "Nopass123")
 
 # Configure machine for ansible remoting
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+Invoke-WebRequest -Uri $url -OutFile $file
 powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP
 
 Set-ItemProperty -Name LocalAccountTokenFilterPolicy -Path HKLM:\software\Microsoft\Windows\CurrentVersion\Policies\system -Value 1
