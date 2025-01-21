044159ffb822f22fbec8f6dc450f825eb79c4d6b
Filip Ciesielski <filip.ciesielski@cyberark.com>
2025-01-21 12:09:14 +0100

    Add signed files

--- tests/integration/default/pester/defaults.tests.ps1
+++
@@ -1,8 +0,0 @@
-describe "ansible_test_kitchen_windows_role ansible role" {
-    Context "PVWA Installation Path" {
-        $Path = "C:\Cyberark\Password Vault Web Access"
-        it "PVWA Directory Exists" {
-            Test-Path -Path $Path | Should be $true
-        }
-    }
-}
